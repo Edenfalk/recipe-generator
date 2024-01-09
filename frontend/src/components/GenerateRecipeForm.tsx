fdd844c0a115df7df46e4b59f17b65e0a006e765
Måns Edenfalk <Medenfalk@gmail.com>
2024-01-09 21:38:32 +0100

    Updated layout for create recipe form

--- frontend/src/components/GenerateRecipeForm.tsx
+++ frontend/src/components/GenerateRecipeForm.tsx
@@ -54,113 +54,158 @@ const GenerateRecipeForm: React.FC<GenerateRecipeFormProps> = ({
 		onRecipeSubmit(data)
 	}
 	return (
-		<div className='flex justify-center items-center p-12'>
-			<Card className='flex flex-col items-center p-8 max-w-md w-full'>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(handleSubmit)}
-						className='max-w-md w-full flex flex-col gap-6'
-					>
-						<FormField
-							control={form.control}
-							name='servings'
-							render={({ field }) => {
-								return (
-									<FormItem>
-										<FormLabel>Servings</FormLabel>
-										<FormControl>
-											<Input
-												placeholder='servings'
-												type='number'
-												min={1}
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)
-							}}
-						/>
-						<FormField
-							control={form.control}
-							name='time'
-							render={({ field }) => {
-								return (
-									<FormItem>
-										<FormLabel>Cooking time</FormLabel>
-										<FormControl>
-											<Input
-												placeholder='Fill in cooking time in minutes'
-												type='number'
-												min={1}
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)
-							}}
-						/>
-						<FormField
-							control={form.control}
-							name='ingredients'
-							render={({ field }) => {
-								return (
-									<div className='flex flex-col gap-2'>
-										<FormLabel>Add ingredients</FormLabel>
-										<div className='flex gap-2'>
-											<FormControl className='flex-grow'>
+		<div className="min-h-screen flex flex-col justify-center items-center p-12 bg-[url('./assets/img/chickenHero.png')] bg-cover bg-no-repeat bg-center mb-6">
+			<h1
+				className='text-6xl font-semibold mb-20'
+				style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)' }}
+			>
+				Start getting creative with AI generated recipes
+			</h1>
+			<Card className='flex lg:flex-row flex-col items-center p-8 max-w-6xl w-full opacity-90 gap-4'>
+				<div className='flex flex-col w-full lg:w-1/2'>
+					<h2 className='text-xl font-semibold mb-4'>Instructions</h2>
+					<ol className='list-decimal list-inside text-left space-y-10 font-semibold'>
+						<li>
+							Fill in the number of servings and the desired
+							cooking time.
+						</li>
+						<li>Add your ingredients, one at a time.</li>
+						<li>Click the 'Create Recipe' button.</li>
+						<li>
+							Wait for the AI to generate a recipe suggestion.
+						</li>
+						<li>
+							If you like the recipe, save and publish it for
+							others to try. An image will be created for your
+							recipe upon saving.
+						</li>
+						<li>Happy Cooking!</li>
+					</ol>
+				</div>
+				<div className='flex flex-col w-full lg:w-1/2'>
+					<h2 className='text-xl font-semibold mb-4'>
+						Create Your Recipe
+					</h2>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(handleSubmit)}
+							className='max-w-md w-full flex flex-col gap-6'
+						>
+							<FormField
+								control={form.control}
+								name='servings'
+								render={({ field }) => {
+									return (
+										<FormItem>
+											<FormLabel className='text-lg'>
+												Servings
+											</FormLabel>
+											<FormControl>
 												<Input
-													placeholder='Add ingredients one by one'
+													className='sm:max-w-full'
+													placeholder='servings'
+													type='number'
+													min={1}
 													{...field}
-													value={currentIngredient}
-													onChange={(e) =>
-														setCurrentIngredient(
-															e.target.value
-														)
-													}
-													onKeyDown={(e) => {
-														if (e.key === 'Enter') {
-															e.preventDefault()
-															addIngredient()
-														}
-													}}
 												/>
 											</FormControl>
-											<Button
-												className='flex-shrink-0'
-												type='button'
-												onClick={addIngredient}
-											>
-												<Plus />
-											</Button>
+											<FormMessage />
+										</FormItem>
+									)
+								}}
+							/>
+							<FormField
+								control={form.control}
+								name='time'
+								render={({ field }) => {
+									return (
+										<FormItem>
+											<FormLabel className='text-lg'>
+												Cooking time
+											</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='Fill in cooking time in minutes'
+													type='number'
+													min={1}
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)
+								}}
+							/>
+							<FormField
+								control={form.control}
+								name='ingredients'
+								render={({ field }) => {
+									return (
+										<div className='flex flex-col gap-2'>
+											<FormLabel className='text-lg'>
+												Add ingredients
+											</FormLabel>
+											<div className='flex gap-2'>
+												<FormControl className='flex-grow'>
+													<Input
+														placeholder='Add ingredients one by one'
+														{...field}
+														value={
+															currentIngredient
+														}
+														onChange={(e) =>
+															setCurrentIngredient(
+																e.target.value
+															)
+														}
+														onKeyDown={(e) => {
+															if (
+																e.key ===
+																'Enter'
+															) {
+																e.preventDefault()
+																addIngredient()
+															}
+														}}
+													/>
+												</FormControl>
+												<Button
+													className='flex-shrink-0'
+													type='button'
+													onClick={addIngredient}
+												>
+													<Plus />
+												</Button>
+											</div>
+											<FormMessage />
 										</div>
-										<FormMessage />
-									</div>
-								)
-							}}
-						/>
-						<div className='flex flex-wrap gap-2'>
-							{ingredients.map((ingredient, index) => (
-								<div
-									key={index}
-									className='flex items-center justify-between border rounded-full p-2'
-								>
-									<span>{ingredient}</span>
-									<button
-										className='ps-3'
-										onClick={() => removeIngredient(index)}
+									)
+								}}
+							/>
+							<div className='flex flex-wrap gap-2'>
+								{ingredients.map((ingredient, index) => (
+									<div
+										key={index}
+										className='flex items-center justify-between border rounded-full p-2'
 									>
-										X
-									</button>
-								</div>
-							))}
-						</div>
-						<Button type='submit' variant='default'>
-							Create Recipe! 🍕
-						</Button>
-					</form>
-				</Form>
+										<span>{ingredient}</span>
+										<button
+											className='ps-3'
+											onClick={() =>
+												removeIngredient(index)
+											}
+										>
+											X
+										</button>
+									</div>
+								))}
+							</div>
+							<Button type='submit' variant='default'>
+								Create Recipe! 🍕
+							</Button>
+						</form>
+					</Form>
+				</div>
 			</Card>
 		</div>
 	)
